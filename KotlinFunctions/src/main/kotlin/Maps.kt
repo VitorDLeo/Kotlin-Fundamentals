d991aaaceda00d333c257f5a2a84de44ad0a35fa
VitorDLeo <vitordileo003@gmail.com>
2022-06-03 00:23:34 -0300

    Maps

--- KotlinFunctions/src/main/kotlin/Maps.kt
+++ KotlinFunctions/src/main/kotlin/Maps.kt
@@ -1,2 +1,14 @@
-class Maps {
+import java.util.*
+
+fun main() {
+    val binaryRepresentation = TreeMap<Char, String>()
+
+    for (c in 'A'..'F'){
+        val binary = Integer.toBinaryString(c.toInt())
+        binaryRepresentation[c] = binary
+    }
+
+    for ((letter, binary) in binaryRepresentation){
+        println("$letter - $binary")
+    }
 }
