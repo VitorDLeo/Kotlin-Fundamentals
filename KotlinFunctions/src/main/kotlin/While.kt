28cfe383a851974e25d779971dd7ec78eae30107
VitorDLeo <vitordileo003@gmail.com>
2022-06-03 00:12:41 -0300

    While

--- KotlinFunctions/src/main/kotlin/While.kt
+++ KotlinFunctions/src/main/kotlin/While.kt
@@ -1,3 +1,18 @@
+
 fun main() {
-    
+    var temp : Int = 0
+    var somatorio : Int = 0
+    var quantidade : Int = 0
+
+    while (temp != 999){
+        println("Digite uma temperatura ou 999 para sair")
+        temp = readLine()!!.toInt()
+
+        if (temp != 999){
+            somatorio += temp
+            quantidade++
+        }
+    }
+
+    println("A media das temperaturas e = ${somatorio / quantidade}")
 }
