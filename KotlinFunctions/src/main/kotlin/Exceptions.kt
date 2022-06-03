9a3543816cf3fb274cca308e93755eb68aef58a9
VitorDLeo <vitordileo003@gmail.com>
2022-06-03 00:39:54 -0300

    Exceptions

--- KotlinFunctions/src/main/kotlin/Exceptions.kt
+++ KotlinFunctions/src/main/kotlin/Exceptions.kt
@@ -1,19 +1,40 @@
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.lang.Exception
+import java.lang.NumberFormatException
+
 fun main() {
     val porcentagem = porcentagem(100)
     println(porcentagem)
+
+    val idade = lerIdade()
+    println(idade)
 }
 
 
 fun porcentagem(numero: Int): String{
     return if (numero in 1..100){
-        "$numero"
+        "$numero%"
     } else {
-        throw IllegalAccessException("$numero deve estar entre 1 e 100")
+        throw IllegalAccessException("numero deve estar entre 1 e 100")
     }
 }
 
-/*
-fun lerIdade(): Int? {
 
+fun lerIdade() : Int? {
+    /*
+    val reader = FileReader(File(pathname:"src/idades.txt"))
+    val buffer = BufferedReader(reader)
+     */
+
+    try{
+        val numero = readLine()
+        return Integer.parseInt(numero)
+    } catch (e: NumberFormatException){
+        return null
+    } finally {
+        readLine()
+    }
 }
- */
+ 
