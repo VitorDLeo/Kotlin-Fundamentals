0a9107f797786c066eb216b2145d82e5e91fec0e
VitorDLeo <vitordileo003@gmail.com>
2022-06-04 20:51:25 -0300

    Functions2

--- KotlinFunctions/src/main/kotlin/Arrays.kt
+++ KotlinFunctions/src/main/kotlin/Arrays.kt
@@ -2,10 +2,15 @@
 fun main() {
 
     var Array1 = arrayOf(1, 2, 3, 4, 5, 6, 7, 8)
+    var funcionarios = arrayOf("Jose", 3000, "Andre", 4000, "Fernando", 6000)
 
     for (i in Array1){
         println(i)
     }
     println("End!")
 
+    println("----------------------------------")
+    for (i in funcionarios){
+        println(i)
+    }
 }
---
+++ KotlinFunctions/src/main/kotlin/Function2.kt
@@ -0,0 +1,11 @@
+
+fun somar (a : Int, b : Int) : Int{
+    var total = a + b
+    return total
+}
+
+fun main() {
+
+    println("A SOMA E IGUAL = ${somar(2, 4)}")
+    
+}
