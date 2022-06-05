d836c1e2143cc93341c6d66507073cb1051b10a5
VitorDLeo <vitordileo003@gmail.com>
2022-06-05 00:35:53 -0300

    Class 2

--- KotlinFunctions/src/main/kotlin/Class2.kt
+++ KotlinFunctions/src/main/kotlin/Class2.kt
@@ -3,9 +3,19 @@ class Casa {
 
     var cor: String = ""
 
-    fun abrirJanela(){
-        println("Janela Aberta")
+    fun abrirJanela(qtdJanelas: Int){
+        println("Quantidade de janelas aberta: $qtdJanelas")
     }
+
+    fun abrirPorta(){
+        println("Porta aberta!!")
+    }
+
+    fun abrirCasa(){
+        // this.abrirJanela()
+        this.abrirPorta()
+    }
+
 }
 
 fun main() {
@@ -13,6 +23,8 @@ fun main() {
     val casa = Casa()
     casa.cor = "Vermelha"
 
-    println(casa.abrirJanela())
+    println(casa.abrirJanela(4))
+
+    println(casa.abrirCasa())
 
 }
